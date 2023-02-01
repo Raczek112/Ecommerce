6c8c1ea1186930bcec398f15ebe11fb27a8e4d7e
Raczek112 <jakubrakowski753@gmail.com>
2023-02-01 12:14:00 +0100

    Created project with basic HTML templates and CSS

---
+++ webshop/shop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'shop/home.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'shop/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'shop/checkout.html', context)
+
